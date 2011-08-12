fa03e3d9d48aac4234725a66f79f60fdf5cc509b
Eric Davis <edavis@littlestreamsoftware.com>
2011-08-12 10:53:33 -0700

    [#6440] Clean up the add icon

--- lib/chiliproject_knowledgebase/hooks/view_issues_history_journal_bottom_hook.rb
+++ lib/chiliproject_knowledgebase/hooks/view_issues_history_journal_bottom_hook.rb
@@ -3,10 +3,10 @@ module ChiliprojectKnowledgebase
     class ViewIssuesHistoryJournalBottomHook < Redmine::Hook::ViewListener
       def view_issues_history_journal_bottom(context={})
         journal_id = context[:journal].id
-        link = content_tag(:a, "Knowledge base", :href => '#', :id => "add-knowledgebase-#{journal_id}", :class => 'icon icon-add knowledgebase-lightbox')
+        link = content_tag(:a, image_tag("book_add.png", :plugin => 'chiliproject_knowledgebase'), :href => '#', :id => "add-knowledgebase-#{journal_id}", :class => 'knowledgebase-lightbox', :title => l(:text_knowledgebase_add))
         html = <<JS
 <script type='text/javascript'>
-$$('#change-#{journal_id} h4 .journal-link').first().insert({ top: '#{link} | '});
+$$('#change-#{journal_id} h4 .journal-link').first().insert({ top: ' #{link} '});
 </script>
 JS
         return html
